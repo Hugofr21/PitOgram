30c029db016168d49a5350f90443f6eda66528cf
HugoRodrigu <1220252@isep.ipp.pt>
2024-10-06 17:13:49 +0100

    update clinte systemd

--- linux/systmed/client/client_start.sh
+++ linux/systmed/client/client_start.sh
@@ -8,7 +8,6 @@ SERVICE_PATH="/etc/systemd/system/${SERVICE_NAME}"
 DIRECTORY_APP="/home/$USER/PitOgram/out/pitOgram-linux-arm64"
 EXEC_PATH="$DIRECTORY_APP/pitogram"
 
-BUMBLE_SERVICE_NAME="bumble-bluetooth.service"
 
 if [ -f "$SERVICE_PATH" ]; then
     echo "Removendo serviço existente..."
@@ -19,13 +18,13 @@ if [ -f "$SERVICE_PATH" ]; then
     sudo systemctl reset-failed
 fi
 
+
 echo "Criando novo arquivo de serviço..."
 sudo bash -c "cat <<EOF > ${SERVICE_PATH}
 [Unit]
 Description=Pitogram
 After=network.target
 Before=graphical.target
-
 
 [Service]
 Type=simple
@@ -36,15 +35,37 @@ Restart=always
 Environment=DISPLAY=:0
 
 [Install]
-WantedBy=graphical.target
+WantedBy=multi-user.target
 EOF"
+
 
 echo "Configurando permissões e habilitando o serviço..."
 sudo chmod 644 ${SERVICE_PATH}
 sudo systemctl daemon-reload
 sudo systemctl enable ${SERVICE_NAME}
 
-echo "Configuração concluída. O Pitogram será iniciado automaticamente após o Bumble Bluetooth."
-echo "Reiniciando o sistema em 5 segundos..."
-sleep 5
+
+echo "Desabilitando o início automático do ambiente gráfico..."
+sudo systemctl set-default multi-user.target
+
+
+echo "Criando script de inicialização..."
+STARTUP_SCRIPT="/home/$USER/start_pitogram_and_desktop.sh"
+sudo bash -c "cat <<EOF > ${STARTUP_SCRIPT}
+#!/bin/bash
+sudo systemctl start ${SERVICE_NAME}
+sleep 2 
+startx  
+EOF"
+sudo chmod +x ${STARTUP_SCRIPT}
+
+
+echo "Configurando .bashrc..."
+echo "if [[ ! \$DISPLAY && \$XDG_VTNR -eq 1 ]]; then" >> /home/$USER/.bashrc
+echo "    exec ${STARTUP_SCRIPT}" >> /home/$USER/.bashrc
+echo "fi" >> /home/$USER/.bashrc
+
+echo "Configuração concluída. O Pitogram será iniciado automaticamente antes do ambiente gráfico."
+echo "Reiniciando o sistema em 10 segundos..."
+sleep 10
 sudo reboot
